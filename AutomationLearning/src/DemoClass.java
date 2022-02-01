c9b05a1c818a0849d29f869a669cd67bfade069c
mbansode <mbansode@xento.com>
2022-02-01 15:51:20 +0530

    first for OOPS

--- AutomationLearning/src/DemoClass.java
+++ AutomationLearning/src/DemoClass.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class DemoClass {
+public class DemoClass {
+    public void show(){
+        System.out.println("in method show");
+      }
+
+      public static void main(String[] args){
+          System.out.println("In main method");
+         //creating object of class
+          DemoClass object=new DemoClass();
+         //calling method
+          object.show();
+      }
 }
